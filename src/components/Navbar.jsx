7d0a061496fa774c2459fc7f113a7608c8ccb22a
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-12-30 18:54:18 +0330

    the login section is created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,13 +26,18 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="customers">
+            <NavLink className="nav-link" to="/customers">
               Customers
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="rentals">
+            <NavLink className="nav-link" to="/rentals">
               Rentals
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              Login
             </NavLink>
           </li>
         </ul>
